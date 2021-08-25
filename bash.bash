d33c70d88e0f541cde9cdad2cf53e5cb197d2231
azaz111 <86608104+azaz111@users.noreply.github.com>
2021-08-25 18:16:15 +0300

    Update bash.bash

--- bash.bash
+++ bash.bash
@@ -83,5 +83,5 @@ cd /root
 # ЗАпуск Плотера ------------------------------
 screen -dmS videorender1 ./chia-plotter/build/chia_plot -n -1 -r 16 -u 256 -t /disk1/vid1/ -2 /disk2/vid2/ -d /disk3/video/ -f b8e1d57e3e2dbb40ac8f2b257b762d05fcfc5b79c32a22255424644b7d183daa7c454624783f2d959c02eb1d2a4ba3a3 -p 91ea997633345082b15f83b957449180037030b6b7485f07ed4ee7558d08d3efbccf2c3d68ba724f5b3a8281a0055e27
 screen -dmS videorender2 ./chia-plotter/build/chia_plot -n -1 -r 16 -u 256 -t /disk1/vid1/ -2 /disk2/vid2/ -d /disk3/video1/ -f b8e1d57e3e2dbb40ac8f2b257b762d05fcfc5b79c32a22255424644b7d183daa7c454624783f2d959c02eb1d2a4ba3a3 -p 91ea997633345082b15f83b957449180037030b6b7485f07ed4ee7558d08d3efbccf2c3d68ba724f5b3a8281a0055e27
-cp ~/bash/root /var/spool/cron/crontabs/
-crontab -e
+python3 vopros.py
+./trans.sh
